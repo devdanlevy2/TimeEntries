11ba7b4f0dc4e1608d02fc72176f304521ffb287
devdanlevy <daniel.levy2@iphone.com>
2016-03-24 08:46:42 -0400

    refactor db setup

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,2 +1,6 @@
 class TimeEntry < ActiveRecord::Base
+  belongs_to :developers
+  validates :duration, presence: true
+  validates :developer_id, presence: true
+  validates :date, presence: true
 end
