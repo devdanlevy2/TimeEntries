c6aa9ec886ada0ab9ebc46fc1128060fd6b6eb24
devdanlevy <daniel.levy2@iphone.com>
2016-03-23 22:40:34 -0400

    work on time_entries MVC

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,19 +1,54 @@
 class TimeEntriesController < ApplicationController
+  before_action :set_time_entry, only: [:show, :edit, :destroy, :update]
+
+  def index
+    @time_entries = TimeEntry.all
+  end
+
   def show
+  end
+
+  def new
+    @time_entries = TimeEntry.new
   end
 
   def edit
   end
 
-  def update
-  end
-
-  def list
-  end
-
-  def delete
-  end
-
   def create
+    @time_entries = TimeEntry.new(time_entry_params)
+
+    if @time_entries.save
+      redirect_to @time_entries, notice: 'TimeEntry was successfully created.'
+    else
+      render :new
+    end
   end
+
+
+
+  def update
+      if @time_entries.update(TimeEntry_params)
+        redirect_to @time_entries, notice: 'TimeEntry was successfully updated.'
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    @time_entries.destroy
+
+      redirect_to TimeEntries_url, notice: 'TimeEntry was successfully destroyed.'
+
+  end
+
+  private
+    def set_TimeEntry
+      @time_entries = TimeEntry.find(params[:id])
+    end
+
+    def TimeEntry_params
+      params.require(:TimeEntry).permit(:project_id, :developer_id, :duration, :date)
+    end
+
 end
